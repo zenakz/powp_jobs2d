12e3014c47854734388b618d6ee0963c1ac7d149
Zenakz <Zenakz@github.com>
2020-03-17 09:46:43 +0100

    Added clearing panel before starting new test

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
@@ -16,6 +17,7 @@ public class SelectTestFigureOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		DrawerFeature.getDrawerController().clearPanel();
 		if(e.getActionCommand().equals("Figure Joe 2"))FiguresJoe.figureScript2(driverManager.getCurrentDriver());
 		else FiguresJoe.figureScript1(driverManager.getCurrentDriver());
 	}
